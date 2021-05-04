726b5eb2f92bb1224d6b30d5a5c32efdf11413ee
Dustin Ruckman <demotis@gmail.com>
2021-05-04 15:31:26 -0500

    Migration of Win32 POE commands to ClientManager

--- PoEClient/ClientManager.cs
+++ PoEClient/ClientManager.cs
@@ -1,5 +1,8 @@
 ﻿using PoE_Trade_Bot.Services;
 using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace PoE_Trade_Bot.PoEClient
 {
@@ -10,6 +13,7 @@ namespace PoE_Trade_Bot.PoEClient
         public static ClientManager Instance => instance;
 
         public bool IsAFK { get; set; }
+        public Process ActiveProcess { get; private set; }
 
         static ClientManager()
         {
@@ -17,13 +21,72 @@ namespace PoE_Trade_Bot.PoEClient
 
         private ClientManager()
         {
-            if (!Win32.IsPoERun())
-            {
+            // Find POE window and setup ActiveProcess
+            Process process = null;
+            if (GetProcess(ConfigManager.Instance.ApplicationConfig["POEProcessName"], out process))
+                ActiveProcess = process;
+            else
                 throw new Exception("Path of Exile is not running!");
-            }
 
         }
 
+        public void ValidateProcess()
+        {
+            if (ActiveProcess == null)
+                throw new Exception("Path of Exile process not set.");
+            if (ActiveProcess.HasExited)
+                throw new Exception("Path of Exile process has been closed.");
+            if (!ActiveProcess.ProcessName.ToLower().Contains(ConfigManager.Instance.ApplicationConfig["POEProcessName"].ToLower()))
+                throw new Exception("Active Process is not Path of Exile.");
+        }
+
+        public void BringToForeground()
+        {
+            ValidateProcess();
+            if (!Win32.SetForegroundWindow(ActiveProcess.MainWindowHandle))
+                throw new Exception($"Unable to set POE Process as foreground window");
+        }
+
+        public bool GetProcess(string processName, out Process process)
+        {
+            process = null;
+            foreach (Process nextProcess in Process.GetProcesses())
+            {
+                if (nextProcess.ProcessName.ToLower().Contains(processName.ToLower()))
+                {
+                    process = nextProcess;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void SendKey(string key)
+        {
+            BringToForeground();
+            SendKeys.SendWait(key);
+        }
+
+        public void ChatCommand(string command)
+        {
+            BringToForeground();
+            SendKeys.SendWait("{ENTER}");
+            foreach (char c in command)
+            {
+                SendKeys.SendWait(c.ToString());
+            }
+            SendKeys.SendWait("{ENTER}");
+        }
+
+        public void SendNumber(int number)
+        {
+            BringToForeground();
+            string str = $"{number}";
+            foreach (Char c in str)
+            {
+                SendKeys.SendWait(c.ToString());
+            }
+        }
         private void Dispose(bool disposing)
         {
             if (!disposedValue)
--- Services/Win32.cs
+++ Services/Win32.cs
@@ -1,33 +1,28 @@
-﻿using PoE_Trade_Bot.Utilities;
-using System;
-using System.Diagnostics;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using System.Windows.Forms;
 
 namespace PoE_Trade_Bot.Services
 {
     public class Win32
     {
-        private static uint X = 0;
-        private static uint Y = 0;
-
-        private static Process PoE_Process = null;
+        public static uint X = 0;
+        public static uint Y = 0;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
         //Mouse actions
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
 
         [DllImport("User32.Dll")]
-        private static extern long SetCursorPos(int x, int y);
+        public static extern long SetCursorPos(int x, int y);
 
         [DllImport("User32.Dll")]
-        private static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
 
         [StructLayout(LayoutKind.Sequential)]
         public struct POINT
@@ -42,6 +37,15 @@ namespace PoE_Trade_Bot.Services
             mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
         }
 
+        public static void ShiftClick()
+        {
+            keybd_event(VK_SHIFT, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+            Thread.Sleep(100);
+            DoMouseClick();
+            Thread.Sleep(100);
+            keybd_event(VK_SHIFT, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
         public static void MoveTo(int x, int y)
         {
             X = Convert.ToUInt32(x);
@@ -50,10 +54,10 @@ namespace PoE_Trade_Bot.Services
         }
 
         [DllImport("user32.dll")]
-        static extern IntPtr GetForegroundWindow();
+        public static extern IntPtr GetForegroundWindow();
 
         [DllImport("user32.dll")]
-        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
         public static string GetActiveWindowTitle()
         {
@@ -72,60 +76,10 @@ namespace PoE_Trade_Bot.Services
         public static extern IntPtr SetFocus(HandleRef hWnd);
 
         [DllImport("user32.dll")]
-        static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        public static void FocusPoEWindow()
-        {
-            if (GetActiveWindowTitle() != "Path of Exile")
-            {
-                Logger.Console.Debug("Focus Path of Exile Window.");
-                PoE_MainWindow();
-            }
-        }
-
-        public static void PoE_MainWindow()
-        {
-            Process[] processList = Process.GetProcesses();
-
-            foreach (Process theProcess in processList)
-            {
-                string processName = theProcess.ProcessName;
-
-                if (processName.ToLower().Contains("pathofexile"))
-                {
-                    PoE_Process = theProcess;
-                    string mainWindowTitle = theProcess.MainWindowTitle;
-                    SetForegroundWindow(theProcess.MainWindowHandle);
-                    break;
-                }
-            }
-
-        }
-
-        public static bool IsPoERun()
-        {
-            Process[] processList = Process.GetProcesses();
-
-            foreach (Process theProcess in processList)
-            {
-                string processName = theProcess.ProcessName;
-
-                if (processName.ToLower().Contains("pathofexile"))
-                {
-                    PoE_Process = theProcess;
-
-                    break;
-                }
-            }
-
-            if (PoE_Process == null)
-                return false;
-            else
-                return true;
-        }
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
 
         [DllImport("user32.dll", SetLastError = true)]
-        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
 
         // Declare some keyboard keys as constants with its respective code
         // See Virtual Code Keys: https://msdn.microsoft.com/en-us/library/dd375731(v=vs.85).aspx
@@ -152,35 +106,6 @@ namespace PoE_Trade_Bot.Services
             DoMouseClick();
             Thread.Sleep(100);
             UpCtrl();
-        }
-
-        public static void SendKeyInPoE(string key)
-        {
-            var proc = PoE_Process;
-            if (proc != null)
-            {
-                var handle = proc.MainWindowHandle;
-                SetForegroundWindow(handle);
-                SendKeys.SendWait(key);
-            }
-        }
-
-        public static void ChatCommand(string command)
-        {
-            var proc = PoE_Process;
-            if (proc != null)
-            {
-                var handle = proc.MainWindowHandle;
-                SetForegroundWindow(handle);
-
-                SendKeys.SendWait("{ENTER}");
-                foreach (char c in command)
-                {
-                    SendKeys.SendWait(c.ToString());
-                }
-                SendKeys.SendWait("{ENTER}");
-
-            }
         }
 
         public static void SetTextClipboard(string text)
@@ -248,25 +173,6 @@ namespace PoE_Trade_Bot.Services
 
             }
             return null;
-        }
-
-        internal static void ShiftClick()
-        {
-            keybd_event(VK_SHIFT, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
-            Thread.Sleep(100);
-            DoMouseClick();
-            Thread.Sleep(100);
-            keybd_event(VK_SHIFT, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-        }
-
-        internal static void SendNumber_PoE(int number)
-        {
-            string str = "" + number;
-
-            foreach (Char c in str)
-            {
-                SendKeys.SendWait(c.ToString());
-            }
         }
     }
 }
